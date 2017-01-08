1156ab8e26fe3eb5c8c0d49b9a46d9a9253f44a4
GrayTurtle <John.Tran@ou.edu>
2017-01-08 17:30:19 -0600

    create model

--- MNIST_Softmax.py
+++ MNIST_Softmax.py
@@ -1,2 +1,11 @@
 import input_data
+import tensorflow as tf
+
+#import data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True);
+
+#create model
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
